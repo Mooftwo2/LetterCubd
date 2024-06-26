f3dff39fc12925e20d822c0ebc2f8eb5a821806a
Mooftwo2 <122929359+Mooftwo2@users.noreply.github.com>
2024-06-25 19:11:21 -0600

    finally fixed rating button position

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include "Geode/binding/CCMenuItemSpriteExtra.hpp"
 #include "Geode/binding/GJAccountManager.hpp"
+#include "Geode/cocos/cocoa/CCGeometry.h"
 #include "Geode/cocos/label_nodes/CCLabelBMFont.h"
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 #include <cstddef>
 #include "ratings_profile.hpp"
 #include "ratings_dict.hpp"
 #include "player_profile.hpp"
+#include "rating_popup.hpp"
 
 using namespace geode::prelude;
 
 class $modify(LetterCubdMenuLayer, MenuLayer) {
-	CCLabelBMFont *label;
-	
+
 	bool init() {
 		if (!MenuLayer::init())
 			return false;
@@ -30,13 +32,9 @@ class $modify(LetterCubdMenuLayer, MenuLayer) {
 		}
 		*/
 		
-		
-		
+	
 		//mapInstance->setRatings(saved);
 
-		label = CCLabelBMFont::create("Hello, World!", "bigFont.fnt");
-		label->setPosition(winSize / 2);
-		this->addChild(label);
 		
 	
 		return true;
@@ -51,3 +49,47 @@ class $modify(LetterCubdMenuLayer, MenuLayer) {
 	
 
 };
+
+
+class $modify(LevelInfoWithRating, LevelInfoLayer) {
+    bool init(GJGameLevel* level, bool challenge) {
+        if (!LevelInfoLayer::init(level, challenge))
+            return false;
+        
+        
+
+        auto menu = CCMenu::create();
+        menu->setID("rating-button");
+		menu->setAnchorPoint({0.f, 0.f});
+		menu->setPosition({0.f, 0.f});
+		menu->ignoreAnchorPointForPosition(true);
+		
+
+        auto spr = CircleButtonSprite::createWithSpriteFrameName("GJ_bigStar_noShadow_001.png");
+        spr->setScale(0.5f);
+        auto btn = CCMenuItemSpriteExtra::create(
+            spr,
+            this,
+            menu_selector(LevelInfoWithRating::onClick)
+        );
+        btn->setID("rating-button");
+        btn->setUserObject(level);
+    
+       	menu->addChild(btn);
+        this->addChild(menu);
+        auto like_count = this->getChildByID("likes-label");
+		btn->setPosition(like_count->getPosition() + CCPoint{(like_count->getContentWidth() * like_count->getScaleX()) + 15.f, 0.f});
+
+        return true;
+    }
+
+    void onClick(CCObject* sender) {
+        
+        auto obj = static_cast<CCNode*>(sender)->getUserObject();
+        auto lvl = static_cast<GJGameLevel*>(obj);
+        RatingPopup::create(lvl)->show();
+
+        
+    }
+};
+
